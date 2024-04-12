996a85a31003b5a2550644cac9ed682047fde878
Caleb Gardner <calebq23@gmail.com>
2024-04-12 02:21:52 -0500

    Chapter 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CalebQ42/Chirpy
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("./server"))))
+	serv := http.Server{
+		Addr:    ":8080",
+		Handler: middlewareCors(mux),
+	}
+	err := serv.ListenAndServe()
+	fmt.Println(err)
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
